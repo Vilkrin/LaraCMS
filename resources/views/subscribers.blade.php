bf86282fcd2e2feccebe256262dfc3fd48ab425d
Vilkrin <contact@vilkrin.uk>
2025-06-07 08:53:01 +0100

    wip

--- resources/views/subscribers.blade.php
+++ resources/views/subscribers.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <div class="container mx-auto px-4 py-8">
+    <div class="container py-8 pt-24 px-4 mx-auto flex-grow">
         <h1 class="text-3xl font-bold text-center mb-6">Subscribers Page</h1>
         <p class="text-center text-gray-600">This page is under construction. Stay tuned for updates!</p>
     </div>
