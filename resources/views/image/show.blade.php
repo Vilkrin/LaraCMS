4789d31f3d323835b46f5c84845598b8daee4b08
Vilkrin <contact@vilkrin.uk>
2025-08-23 14:44:57 +0100

    update of image display on single gallery image display

--- resources/views/image/show.blade.php
+++ resources/views/image/show.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
-
-<img class="h-auto max-w-xl mx-auto rounded-lg shadow-xl dark:shadow-gray-800" src="{{ $photo->getFirstMediaUrl('images') }}" alt="Image">
-
+    <div class="pt-24 flex justify-center">
+        <img class="h-auto max-w-3xl rounded-lg shadow-xl dark:shadow-gray-800" 
+             src="{{ $photo->getFirstMediaUrl('images') }}" 
+             alt="Image">
+    </div>
 </x-layout>
