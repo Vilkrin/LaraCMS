812ef4d430e809ea93bc51e272c52ea36914854c
Vilkrin <contact@vilkrin.uk>
2026-08-13 09:54:53 +0100

    wip
    wip

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
-Route::view('/welcome', 'welcome')->name('welcomegit add .');
+Route::view('/welcome', 'welcome')->name('welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
